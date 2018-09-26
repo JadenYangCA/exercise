59fa5bb2a74a551af01635a4668d0b3304d2bef7
JadenYangCA <jaden.j.yang@gmail.com>
2018-09-26 15:57:11 -0600

    startup the app using environment variables.

---
+++ application/startup_app.bat
@@ -0,0 +1,9 @@
+set "EXERCISE_APP_HOME=D:/application"
+
+set server.port=8081
+set "server.context-path=/exercise"
+set server.tomcat.uri-encoding=UTF-8
+set "appconfig.filePath=%EXERCISE_APP_HOME%/king-i.txt"
+set "logging.path=%EXERCISE_APP_HOME%/log"
+
+java -jar "%EXERCISE_APP_HOME%/exercise-0.0.1-SNAPSHOT.jar"
